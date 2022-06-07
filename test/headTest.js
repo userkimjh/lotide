4c5b6f32dac5de87452c8984d9e69dd367d58542
John Kim <96427720+userkimjh@users.noreply.github.com>
2022-06-07 10:39:58 -0400

    added head and tail test cases

--- test/headTest.js
+++ test/headTest.js
@@ -2,6 +2,5 @@ const assertEqual = require('../assertEqual');
 const head = require('../head');
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("1", 1);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
